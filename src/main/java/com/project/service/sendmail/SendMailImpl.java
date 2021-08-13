148a83bc57a1851e37991a0afe40fe1bd6af383a
Do The Tai <dothetaind00@gmail.com>
2021-08-14 01:18:36 +0700

    error merge branch of minh into master

--- src/main/java/com/project/service/sendmail/SendMailImpl.java
+++ src/main/java/com/project/service/sendmail/SendMailImpl.java
@@ -1,8 +1,6 @@
 package com.project.service.sendmail;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
+import lombok.var;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
